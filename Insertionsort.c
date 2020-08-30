402cdce368168840983e29d3436e2263eb2f7a6b
kunalznk <50258809+kunalznk@users.noreply.github.com>
2020-08-30 14:34:02 +0530

    Update Insertionsort.c

--- Insertionsort.c
+++ Insertionsort.c
@@ -6,10 +6,12 @@
 
 int main()
 {
-	int a[11];
+	int a[11]; //Static Array OF Size 11
 	int n =11;
-	int iteration = 0;
+	int iteration = 0; // No Of Iteration To Peform For Sorting the list
 	
+	
+	// Taking the INPUT from the Console
 	for(int i=0;i<n;i++){
 		
 		scanf("%d",&a[i]);
@@ -18,8 +20,8 @@ int main()
 	
 	for(int i =1 ; i<n; i++){
 		
-		int hole = a[i];
-		iteration = iteration + 1;
+		int hole = a[i];                       // As INSERTION SORT WORKS BY DIVIDING ARRAY INTO PART SORTED AND SORTED
+		iteration = iteration + 1;             // HOLE IS THE CURRENT ELEMENT WHICH IS TO BE INSERT IN APPROPRIATE POSTION
 	
 	    for(int j =i; (j>0) && a[j-1]>a[j];j--){
 			
